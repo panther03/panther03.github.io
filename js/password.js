92a340219dfd6fa0f174243c61728ab7b8256e59
panther03 <tripod320@gmail.com>
2018-06-05 21:18:26 -0500

    no more peepers on my website tryna steal the secret resources 😤😤

--- js/password.js
+++ js/password.js
@@ -1,7 +1,7 @@
 function pass() {
     var pass_1 = document.forms["passwordForm"].password.value;
-    var correct_pass = "password"
-    if (pass_1 == correct_pass) {
+    var correct_pass_hash = "20c1ef2d7a61850be949d3fb6d838b75"
+    if (hex_md5(pass_1) == correct_pass_hash) {
         window.location.replace("secret_resources.html");
     } else {
       alert("try again it's wrong");
